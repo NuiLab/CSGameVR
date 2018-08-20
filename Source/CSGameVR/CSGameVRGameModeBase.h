ca66a84ec5cf2974c9838b2b42bc8f0ab0088c67
Fidel Hernandez <fhern103@fiu.edu>
2018-08-20 16:09:03 -0400

    Initial commit

---
+++ Source/CSGameVR/CSGameVRGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CSGameVRGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CSGAMEVR_API ACSGameVRGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
